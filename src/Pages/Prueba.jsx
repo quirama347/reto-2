53b2a9d557c49c89314f37acd5ec6885419b69a5
jhonatanquirama <quiramapino47@gmail.com>
2020-09-21 17:53:50 -0500

    actualizaciones

---
+++ src/Pages/Prueba.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Register from '../Components/Register/RegisterUser'
+
+export class Prueba extends Component {
+    render() {
+        return (
+            <div>
+                <Register/>
+            </div>
+        )
+    }
+}
+
+export default Prueba
